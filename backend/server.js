99f050bff82c43717e2715b68a49c10988563b32
Zahin-commits <zahinhosen@gmail.com>
2024-03-27 23:39:19 +0600

    installed socket.io

--- backend/server.js
+++ backend/server.js
@@ -2,26 +2,36 @@ require('dotenv').config('./');
 const express = require("express");
 const cookieParser = require('cookie-parser');
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3001;
 const cors = require('cors');
-const connectDB = require('./config/db');
 const authRoute = require('./routers/authRoute');
 const groupRoute = require('./routers/GroupRoute');
 const messageRoute = require('./routers/messageRoute');
 const { protect } = require('./middleware/auth');
+const {createServer} = require('http');
+const {Server} = require('socket.io');
+const connectDB = require('./config/db');
 
 connectDB();
+
+const server = createServer(app);
+const io = new Server(server,{
+    cors: {
+      origin: "http://localhost:3006"
+    }
+  });
+
+app.use(cors({
+ origin:'http://localhost:3006',
+ credentials:true
+}))
 
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 
 app.use(cookieParser());
 
-app.use(cors({
-    credentials: true,
-    origin:"http://localhost:3006"
-}
-));
+
 
 app.get('/',(req,res)=>{
  res.json('the server is online');
@@ -35,6 +45,57 @@ app.get('/private',protect,(req,res)=>{
  res.json('this is a private route');
 });
 
-app.listen(PORT,()=>{
- console.log(`the server is listening at port ${PORT}...`);
+
+global.onlineUsers = new Map();
+const groups = {};
+
+io.on('connection',(socket)=>{
+  global.chatScoket = socket;
+
+    console.log('a user connected');
+    socket.on('add-user',(userId)=>{
+     onlineUsers.set(userId,socket.id);
+     console.log('add user',onlineUsers);
+    })
+
+    socket.on('send-dm-message',(data)=>{
+     const sendUserSocket = onlineUsers.get(data.to);
+   //  console.log(data);
+
+     if(sendUserSocket){
+       console.log('server recived dm msg',data.text);
+       socket.to(sendUserSocket).emit("msg-recived",{
+         from:data.from,
+         to:data.to,
+         username:data.username,
+       text:data.text
+       });
+     }
+    })
+ 
+    socket.on('joinGroup', ({ groupId, userId }) => {
+     socket.join(groupId);
+     if (!groups[groupId]) {
+       groups[groupId] = [];
+     }
+     groups[groupId].push(socket.id);
+   });
+
+   socket.on('send-groupMessage', (data) => {
+    // io.to(data.to).emit('recived-groupMessage', { 
+    socket.to(data.to).emit('recived-groupMessage', { 
+       from:data.from,
+       to:data.to,
+       text:data.text
+        });
+   });
+
+    socket.on('disconnect',()=>{
+    console.log('a user disconnected');
+   })
+})
+
+
+server.listen(PORT,()=>{
+console.log(`the server is littening at port ${PORT}...`);
 })
--- frontend/src/screens/Home.jsx
+++ frontend/src/screens/Home.jsx
@@ -1,11 +1,24 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import { LeftBar } from '../components/LeftBar'
 import { ChatContainer } from '../components/ChatContainer'
+import { useNavigate } from 'react-router-dom'
+import {socket} from '../socket'
 
 export const Home = () => {
   const [selectedUser, setSelectedUser] = useState([]);
   const [selectedGroup, setSelectedGroup] = useState([]);
+  const navigate = useNavigate();
 
+  const userInfo = JSON.parse(localStorage.getItem('userInfo')) || ''; 
+ 
+  useEffect(()=>{
+    if(!localStorage.getItem('userInfo')){
+       navigate('/');
+    }else{
+     socket.emit('add-user',userInfo._id);
+     console.log('userinfo',userInfo)
+    }
+  },[])
   return (
     <div id='home'>
       <LeftBar setSelectedUser={setSelectedUser} setSelectedGroup={setSelectedGroup} />
