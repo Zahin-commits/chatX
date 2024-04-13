0f7ea78711aca083c529eb7dff3fb463f4afa7c2
Zahin-commits <zahinhosen@gmail.com>
2024-04-13 19:58:21 +0600

    url fix

--- frontend/src/socket.js
+++ frontend/src/socket.js
@@ -1,5 +1,6 @@
 import {io} from 'socket.io-client';
 
-const URL = 'http://localhost:3000/';
+// const URL = 'http://localhost:3000/';
+const URL = 'https://chatx-gx3j.onrender.com/';
 
 export const socket = io(URL);
